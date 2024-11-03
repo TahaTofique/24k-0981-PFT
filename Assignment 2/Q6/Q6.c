e1588a7ba19f7c1cdd092258352bf03e0ae5d68d
Taha Tofique <tahatofique2005@gmail.com>
2024-11-03 11:47:38 +0500

    Create Q6.c

---
+++ Assignment 2/Q6/Q6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int match(int n){
+    if(n%5==0){
+        return -1;
+    }else {
+        return n%5;
+    }
+}
+
+int main()
+{
+    int n;
+     printf("Enter the number of matchsticks : ");
+    scanf("%d",&n);
+    match(n);
+    int result=match(n);
+    if(result==-1){
+        printf("Return -1");
+    }else{
+        printf("A should pick %d matchsticks\n",result);
+    }
+
+}
